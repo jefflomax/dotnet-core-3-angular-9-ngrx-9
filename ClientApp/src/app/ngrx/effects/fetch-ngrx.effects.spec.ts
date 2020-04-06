4f10730fcba7f275d7089e503e15c024628ca0f4
jlomax-techshare <jeff.lomax@cuc.org>
2020-04-06 13:34:39 -0500

    Effect created test

--- ClientApp/src/app/ngrx/effects/fetch-ngrx.effects.spec.ts
+++ ClientApp/src/app/ngrx/effects/fetch-ngrx.effects.spec.ts
@@ -1,22 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { WeatherForecastService } from '@services/weather-forecast.service';
 
 import { FetchNgrxEffects } from './fetch-ngrx.effects';
 
-xdescribe('FetchNgrxEffects', () => {
+describe('FetchNgrxEffects', () => {
+  let httpClientMock: HttpClient;
+  // tslint:disable-next-line: prefer-const
   let actions$: Observable<any>;
   let effects: FetchNgrxEffects;
+  let forecastsMock: WeatherForecastService;
 
   beforeEach(() => {
+    const fakeHandler: any = null;
+    httpClientMock = new HttpClient(fakeHandler);
+    forecastsMock = new WeatherForecastService(httpClientMock, '');
+
     TestBed.configureTestingModule({
       providers: [
+        {provide: HttpClient, useValue: httpClientMock },
+        {provide: WeatherForecastService, useValue: forecastsMock},
         FetchNgrxEffects,
         provideMockActions(() => actions$)
       ]
     });
 
-    effects = TestBed.get<FetchNgrxEffects>(FetchNgrxEffects);
+    effects = TestBed.inject<FetchNgrxEffects>(FetchNgrxEffects);
   });
 
   it('should be created', () => {
